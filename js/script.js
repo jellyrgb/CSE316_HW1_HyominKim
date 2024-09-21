d920b551b0dc99e2b6c182aa8feb674d97dc6485
Hyomin <hyomin.kim@stonybrook.edu>
2024-09-22 07:25:40 +0900

    header update

--- js/script.js
+++ js/script.js
@@ -1,25 +1,80 @@
-// Sample facility list data
 const facilities = [
-    { id: "gym", name: "Gym", capacity: 100, image: "images/gym.jpg" },
-    { id: "auditorium", name: "Auditorium", capacity: 300, image: "images/auditorium.jpg" },
-    { id: "conference", name: "Conference Room", capacity: 50, image: "images/conference.jpg" },
-    { id: "library", name: "Library", capacity: 200, image: "images/library.jpg" },
-    { id: "pool", name: "Pool", capacity: 30, image: "images/pool.jpg" },
-    { id: "seminar", name: "Seminar Room", capacity: 20, image: "images/seminar.jpg" }
+    { id: "gym", name: "Gym", description: "sports hall", capacity: 4, location: "C1033", availability: "Available to all", image: "images/gym.jpg" },
+    { id: "auditorium", name: "Auditorium", description: "the auditorium theater", capacity: 30, location: "A234", availability: "Available to all", image: "images/auditorium.jpg" },
+    { id: "pool", name: "Swimming Pool", description: "aqautic center", capacity: 4, location: "C1033", availability: "Available to all", image: "images/pool.jpg" },
+    { id: "seminar", name: "Seminar Room", description: "lecture hall", capacity: 4, location: "C1033", availability: "Available to all", image: "images/seminar.jpg" },
+    { id: "conference", name: "Conference Room", description: "meeting space", capacity: 4, location: "C1033", availability: "Only for SUNY Korea", image: "images/conference.jpg" },
+    { id: "library", name: "Library", description: "study and read books", capacity: 4, location: "C1033", availability: "Only for SUNY Korea", image: "images/library.jpg" },
 ];
 
-// Populate facility list page
+
 const facilityList = document.getElementById('facility-list');
 if (facilityList) {
     facilities.forEach(facility => {
+        displayFacilityInfo(facility.id);
+    });
+}
+
+// Event listener for facility selection
+document.getElementById('facility').addEventListener('change', function() {
+    console.log("OKAY")
+    const facilityDisplay = document.getElementById('facility-display');
+    if (facilityDisplay) {
+        displayFacilityInfo(this.value);
+    }
+});
+
+// Initial display of facility information
+document.addEventListener('DOMContentLoaded', function() {
+    const initialFacility = document.getElementById('facility').value;
+    displayFacilityInfo(initialFacility);
+});
+
+const facilitySelect = document.getElementById('facility');
+if (facilitySelect) {
+    displayFacilityInfo(facilitySelect.value);
+}
+
+function displayFacilityInfo(facilityId) {
+    const facility = facilities.find(f => f.id === facilityId);
+    const facilityInfoDiv = document.getElementById('facility-display');
+    if (facility) {
         const facilityItem = document.createElement('div');
+        facilityItem.classList.add('facility-item');
         facilityItem.innerHTML = `
             <img src="${facility.image}" alt="${facility.name}">
-            <h2>${facility.name}</h2>
-            <p>Capacity: ${facility.capacity}</p>
+            <div class="facility-info">
+                <h2>${facility.name}</h2>
+                <p>${facility.description}</p>
+
+                <div class="capacity">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-people-fill" viewBox="0 0 16 16">
+                    <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5.784 6A2.24 2.24 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.3 6.3 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5"/>
+                    </svg>
+                    <span>${facility.capacity}</span>
+                </div>
+
+                <div class="location">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pin-map-fill" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M3.1 11.2a.5.5 0 0 1 .4-.2H6a.5.5 0 0 1 0 1H3.75L1.5 15h13l-2.25-3H10a.5.5 0 0 1 0-1h2.5a.5.5 0 0 1 .4.2l3 4a.5.5 0 0 1-.4.8H.5a.5.5 0 0 1-.4-.8z"/>
+                    <path fill-rule="evenodd" d="M4 4a4 4 0 1 1 4.5 3.969V13.5a.5.5 0 0 1-1 0V7.97A4 4 0 0 1 4 3.999z"/>
+                    </svg>
+                    <span>${facility.location}</span>
+                </div>
+
+                <div class="availability">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-circle" viewBox="0 0 16 16">
+                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                    <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0M7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z"/>
+                    </svg>
+                    <span>${facility.availability}</span>
+                </div>
+            </div>
         `;
         facilityList.appendChild(facilityItem);
-    });
+    } else {
+        facilityInfoDiv.innerHTML = '<p>No information available.</p>';
+    }
 }
 
 // Reservation form logic
